7ac585ef0c208e6c05061836dc820002afff6f9c
T.J. Daddio <git@tylerdaddio.com>
2019-04-11 13:57:23 -0500

    Add fetch caching and remove only.util.injectHTML
    Add fetch caching. only.utils.fetch and only.utils.fetchAndFill previously re-fetched from the provided URL at every invokation. This is not always desired, especially when server bandwidth is limited. This commit introduces the useCache parameter to both functions. When set to true, the response from the first fetch of a URL will be re-used. To force a new fetch (useful when the remote resource has been updated, for example), set useCache to false.
    
    Remove only.util.injectHTML. It only performs node.innerHTML=text, which clearly can be done in a single line. It would be more useful as an alternative to only.fetch.fetchAndFill: only.fetch.fetchAndAppend. This may be added in a future update.

--- only-utils.js
+++ only-utils.js
@@ -4,30 +4,49 @@
 
 only.utils = {};
 
-only.utils.fetch = (url) => {
+var cache = {};
+
+/**
+ * Fetches from a URL optionally caching the response received.
+ * @param url       URL to fetch from.
+ * @param useCache  Default true. Provide false to force a fetch.
+ */
+only.utils.fetch = (url, useCache=true) => {
+	if(url in cache && useCache) {
+		var resp = cache[url];
+		return Promise.resolve(resp.clone());
+	}
 	return fetch(url)
 	.then(resp => {
+		cache[url] = resp.clone();
 		if(resp.status == 404)
 			throw Error("404 no response");
 		return resp;
 	});
 };
 
-only.utils.fetchAndFill = (url, node) => {
-	return only.utils.fetch(url)
+/**
+ * Fetches from a URL, gets the text from the body of the response, then sets
+ * the inner HTML of the node. Optionally caches the response received.
+ * @param url       URL to fetch from.
+ * @param node      Node to fill.
+ * @param useCache  Default true. Provide false to force a fetch.
+ */
+only.utils.fetchAndFill = (url, node, useCache=true) => {
+	return only.utils.fetch(url, useCache)
 		.then((resp) => resp.text())
 		.then((html) => {
 			node.innerHTML = html;
 		});
 };
 
+/**
+ * Removes all children from a node.
+ * @param node  Node to empty.
+ */
 only.utils.emptyNode = (node) => {
 	while(node.hasChildNodes())
 		node.removeChild(node.lastChild);
-};
-
-only.utils.injectHTML = (node, html) => {
-	node.innerHTML += html;
 };
 
 })();
